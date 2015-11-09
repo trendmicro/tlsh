7307dd6e5c7827ae9653d1fe4df5869df573b537
Scott4man <lane_forman@trendmicro.com>
2015-11-09 15:04:35 -0800

    Add regression tests for python modules

--- py_ext/test.py
+++ py_ext/test.py
@@ -3,7 +3,13 @@ from __future__ import print_function
 import sys
 import tlsh
 
-def compute(path):
+def compute_1(path):
+    with open(path, 'rb') as f:
+        data = f.read()
+        hs = tlsh.hash(data)
+    return hs
+
+def compute_2(path):
     h = tlsh.Tlsh()
     with open(path, 'rb') as f:
         for buf in iter(lambda: f.read(512), b''):
@@ -11,14 +17,19 @@ def compute(path):
     h.final()
     return h
 
-h1 = compute(sys.argv[1])
-hex1 = h1.hexdigest()
-print('hex1', hex1)
-h2 = compute(sys.argv[2])
-hex2 = h2.hexdigest()
-print('hex2', hex2)
+hex1 = compute_1(sys.argv[1])
+print('tlsh.hash hex1', hex1)
+hex2 = compute_1(sys.argv[2])
+print('tlsh.hash hex2', hex2)
 print('tlsh.diff(hex1, hex2)', tlsh.diff(hex1, hex2))
 print('tlsh.diff(hex2, hex1)', tlsh.diff(hex2, hex1))
+
+h1 = compute_2(sys.argv[1])
+hex1 = h1.hexdigest()
+print('tlsh.Tlsh hex1', hex1)
+h2 = compute_2(sys.argv[2])
+hex2 = h2.hexdigest()
+print('tlsh.Tlsh hex2', hex2)
 print('h1.diff(h2)', h1.diff(h2))
 print('h2.diff(h1)', h2.diff(h1))
 print('h1.diff(hex2)', h1.diff(hex2))
