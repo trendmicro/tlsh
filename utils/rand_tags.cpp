fb076b0f62b620e0de4cf95a5ccb81f21d4abc24
JonO <jono@locals-MacBook-Pro.local>
2017-09-01 09:47:30 +0200

    resolving issue #38

--- utils/rand_tags.cpp
+++ utils/rand_tags.cpp
@@ -366,7 +366,7 @@ struct tagdef oneoff_def[] = {
 
 static void rhtml_contents(std::string &htmls, int *ntags, int *ndistinct_tags)
 {
-	if ((*ntags <= 0) && (ndistinct_tags <= 0))
+	if ((*ntags <= 0) && (*ndistinct_tags <= 0))
 		return; 
 	if (random() % 10 == 1) {
 		anchor(htmls);
