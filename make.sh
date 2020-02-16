04a13d43c6ad79bb33df736a872eb93d650d2558
John Hood <cgull@glup.org>
2020-02-15 19:24:33 -0500

    Improve shared library generation
    Make it possible to build shared libraries without source edits.
    Use the major version number on major-version-only solib name.
    Change make.sh to use the new variable.

--- make.sh
+++ make.sh
@@ -16,11 +16,11 @@ fi
 if [ $# -eq 1 -a "$1" = "debug" ]; then
   mkdir -p build/debug
   cd build/debug
-  cmake -DCMAKE_BUILD_TYPE=Debug ../..
+  cmake -DTLSH_SHARED_LIBRARY=1 -DCMAKE_BUILD_TYPE=Debug ../..
 else
   mkdir -p build/release
   cd build/release
-  cmake ../.. 
+  cmake -DTLSH_SHARED_LIBRARY=1 ../.. 
 fi
 makecversion=0
 if [ $# -eq 1 -a "$1" = "-c" ]; then
