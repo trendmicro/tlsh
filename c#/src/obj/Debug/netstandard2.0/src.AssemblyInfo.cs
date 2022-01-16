f00d6fab3758e964a65fdc85705b6979381eac5b
Krause, Henning <Henning.Krause@netatwork.de>
2022-01-16 15:06:39 +0100

    Removed temporary files

--- c#/src/obj/Debug/netstandard2.0/src.AssemblyInfo.cs
+++ c#/src/obj/Debug/netstandard2.0/src.AssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Henning Krause")]
+[assembly: System.Reflection.AssemblyCompanyAttribute(" ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("TLSH is a fuzzy matching library. Given a byte stream with a minimum length of 50" +
     " bytes TLSH generates a hash value which can be used for similarity comparisons." +
