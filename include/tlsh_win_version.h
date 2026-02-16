21c47b98d67599cda32440ac1b8a229ecf1a238b
Jonathan Oliver <jono@MacBook-Pro.local>
2026-02-17 10:21:01 +1100

    **4.13.4**
    17/02/2026
            Resolve #162 Reverse out T1 change made in 4.13.1

--- include/tlsh_win_version.h
+++ include/tlsh_win_version.h
@@ -7,7 +7,7 @@
 
 #define VERSION_MAJOR		4
 #define VERSION_MINOR		13
-#define VERSION_PATCH		3
+#define VERSION_PATCH		4
 #define TLSH_HASH		"compact hash"
 #define TLSH_CHECKSUM		"1 byte checksum"
 
